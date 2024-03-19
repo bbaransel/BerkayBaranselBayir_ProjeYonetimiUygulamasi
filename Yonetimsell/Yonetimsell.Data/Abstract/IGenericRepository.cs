26f4d6ff783cd515bcf12845ffe3c939284fea99
bbaransel <baran9932@gmail.com>
2024-03-19 15:31:16 +0300

    Data layer CRUD
    GenericRepository and CRUD methods on data layer

---
+++ Yonetimsell/Yonetimsell.Data/Abstract/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yonetimsell.Data.Abstract
+{
+    internal interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> options = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> options = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task HardDeleteAsync(TEntity entity);
+        
+        
+    }
+}
---
+++ Yonetimsell/Yonetimsell.Data/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Yonetimsell.Data.Abstract;
+
+namespace Yonetimsell.Data.Concrete.Repositories
+{
+    public class GenericRepository<TEntity>:IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext _dbContext;
+
+        public GenericRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> options = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null)
+        {
+            IQueryable<TEntity> query =  _dbContext.Set<TEntity>();
+            if(include != null)
+            {
+                query= include(query);
+            }
+            if(options != null)
+            {
+                query = query.Where(options);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> options = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null)
+        {
+            IQueryable<TEntity> query = _dbContext.Set<TEntity>();
+            if (include != null)
+            {
+                query = include(query);
+            }
+            if (options != null)
+            {
+                query = query.Where(options);
+            }
+            return await query.SingleOrDefaultAsync();
+        }
+
+        public async Task HardDeleteAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+            
+        }
+    }
+}
